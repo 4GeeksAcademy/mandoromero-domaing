cc256423404885c0a5f623eeffbc3dd3181b6d43
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-04-24 14:30:06 +0000

    domains

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainList = [];
+  let pronoun = ["His", "Her"];
+  let adj = ["big", "small"];
+  let noun = ["wolf", "sheep"];
+  let domainNames = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domainNames.length; l++) {
+          domainList.push(pronoun[i] + adj[j] + noun[k] + domainNames[l]);
+        }
+      }
+    }
+  }
+  console.log(domainList);
 };
